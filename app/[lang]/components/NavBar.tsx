92cb296247c90d99668c9a186d16210a03b34c0b
abdalraoufalsheikhnajeeb <abdalraoufalsheikhnajeeb@gmail.com>
2024-07-19 11:09:17 +0300

    d

--- app/[lang]/components/NavBar.tsx
+++ app/[lang]/components/NavBar.tsx
@@ -1,11 +1,11 @@
 "use client";
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-import { useState, useEffect } from "react";
-import { useMediaQuery } from "react-responsive";
 import { type getDictionary } from "../../../get-dictionary";
 import LocaleSwitcher from "./locale-switcher";
+import useMediaQuery from "./useMediaQuery";
 
 const Navbar = ({
   dic,
@@ -14,18 +14,16 @@ const Navbar = ({
 }) => {
   const [isBurgerOpen, setIsBurgerOpen] = useState(false);
   const lang = dic?.currLang;
-  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
-  const toggleBurger = () => {
-    setIsBurgerOpen(!isBurgerOpen);
-  };
+  const isMobile = useMediaQuery("(max-width: 1024px)");
+  const pathname = usePathname();
 
   useEffect(() => {
-    // Retrieve the selectedPageIndex from localStorage on component mount
     const storedPageIndex = localStorage.getItem("selectedPageIndex");
     if (storedPageIndex !== null) {
-      setSelectedPageIndex(parseInt(storedPageIndex));
+      setSelectedPageIndex(parseInt(storedPageIndex, 10));
     }
   }, []);
+
   useEffect(() => {
     const handleResize = () => {
       if (!isMobile) {
@@ -39,101 +37,70 @@ const Navbar = ({
   }, [isMobile]);
 
   const [selectedPageIndex, setSelectedPageIndex] = useState(0);
-  const pathname = usePathname();
-  console.log("pathname", pathname);
-  return (
-    <nav
-      className={`bg-gray-100 backdrop-brightness-125 h-20 w-full px-[5vw] py-2 flex items-center top-0 justify-between z-[100] fixed`}
-    >
-      <Link href={`/${lang}`}>
 
-      <Image
-        loading="lazy"
-        quality={1}
-        className="flex-shrink-0 h-full"
-        width={100}
-        height={80}
-        src="/images/logo.png"
-        alt="Logo"
-      />
+  const toggleBurger = () => {
+    setIsBurgerOpen(!isBurgerOpen);
+  };
+
+  const closeBurgerMenu = () => {
+    setIsBurgerOpen(false);
+  };
+
+  const NavItem = ({ href, label }: { href: string; label: string }) => (
+    <Link href={href}
+     
+        className={`text-primary text-xl z-20 relative hover:bg-primary-500/40 px-3 py-2 rounded-md ${
+          pathname === href ? "underline underline-offset-[12px] font-bold" : ""
+        }`}
+        onClick={closeBurgerMenu}
+      >
+        {label}
+    
+    </Link>
+  );
+
+  return (
+    <nav className="bg-gray-100 backdrop-brightness-125 h-20 w-full px-[5vw] py-2 flex items-center top-0 justify-between z-[100] fixed">
+      <Link href={`/${lang}`}>
+        
+          <Image
+            loading="lazy"
+            quality={10}
+            className="flex-shrink-0 h-16 w-28"
+            width={100}
+            height={80}
+            src="/images/logo.png"
+            alt="Logo"
+          />
+  
       </Link>
 
-
-      <div className="hidden lg:flex ms-10  items-baseline space-x-4">
-        <Link
-          className={`text-primary text-xl z-20 relative hover:bg-primary-500/40  px-3 py-2 rounded-md  ${
-            pathname === `/${lang}`
-              ? "underline underline-offset-[12px] font-bold"
-              : ""
-          }`}
-          href={`/${lang}`}
-        >
-          {dic?.Home}
-        </Link>
-        <Link
-          className={`text-primary text-xl z-20 relative hover:bg-primary-500/40  px-3 py-2 rounded-md  ${
-            pathname === `/${lang}/about-us`
-              ? "underline underline-offset-8 font-bold"
-              : ""
-          }`}
-          href={`/${lang}/about-us`}
-        >
-          {dic?.aboutUs}
-        </Link>
-        <Link
-          className={`text-primary text-xl z-20 relative hover:bg-primary-500/40  px-3 py-2 rounded-md  ${
-            pathname === `/${lang}/WorldClock`
-              ? "underline underline-offset-8 font-bold"
-              : ""
-          }`}
-          href={`/${lang}/WorldClock`}
-        >
-          <span>{dic.worldClock}</span>
-        </Link>
+      <div className="hidden lg:flex ms-10 items-baseline space-x-4">
+        <NavItem href={`/${lang}`} label={dic?.Home} />
+        <NavItem href={`/${lang}/about-us`} label={dic?.aboutUs} />
+        <NavItem href={`/${lang}/WorldClock`} label={dic.worldClock} />
       </div>
+
       <div className="flex items-center h-14 gap-4">
         <LocaleSwitcher />
 
         <Image
           loading="lazy"
           quality={1}
-          className={`cursor-pointer h-10 shrink-0  z-10 -me-2 flex lg:hidden`}
+          className="cursor-pointer h-10 shrink-0 z-10 -me-2 flex lg:hidden"
           onClick={toggleBurger}
           src={isBurgerOpen ? "/images/Cart.svg" : "/images/burger_menu.svg"}
           width={50}
           height={50}
-          alt="language"
+          alt="Toggle menu"
         />
       </div>
+
       {isBurgerOpen && (
         <div className="flex bg-white w-screen h-screen flex-col fixed top-[60vh] left-1/2 transform -translate-x-1/2 -translate-y-1/2 gap-4 justify-center items-center z-50">
-          <Link
-            className={`text-primary text-xl z-20 relative hover:bg-primary-500/40 hover:text-primary px-3 py-2 rounded-md  ${
-              pathname === "/" ? "active" : ""
-            }`}
-            href={`/${lang}`}
-            onClick={() => setIsBurgerOpen(false)}
-          >
-            {dic?.Home}
-          </Link>
-          <Link
-            className={`text-primary text-xl z-20 relative hover:bg-primary-500/40 hover:text-primary px-3 py-2 rounded-md  ${
-              pathname === "/about-us" ? "active" : ""
-            }`}
-            href={`/${lang}/about-us`}
-            onClick={() => setIsBurgerOpen(false)}
-          >
-            {dic?.aboutUs}
-          </Link>
-          <Link
-            className={`text-primary text-xl z-20 relative hover:bg-primary-500/40 hover:text-primary px-3 py-2 rounded-md  ${
-              pathname === `/${lang}/WorldClock` ? "active" : ""
-            }`}
-            href={`/${lang}/WorldClock`}
-            onClick={() => setIsBurgerOpen(false)}
-          >
-            <span>{dic.worldClock}</span>
-          </Link>
+          <NavItem href={`/${lang}`} label={dic?.Home} />
+          <NavItem href={`/${lang}/about-us`} label={dic?.aboutUs} />
+          <NavItem href={`/${lang}/WorldClock`} label={dic.worldClock} />
         </div>
       )}
     </nav>
--- app/[lang]/components/locale-switcher.tsx
+++ app/[lang]/components/locale-switcher.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { i18n, type Locale } from "../../../i18n-config";
+import { type Locale } from "../../../i18n-config";
 import Image from "next/image";
 
 export default function LocaleSwitcher() {
@@ -14,44 +14,42 @@ export default function LocaleSwitcher() {
   };
 
   return (
-    <ul className="flex gap-3">
-      <li>
-        <Link href={redirectedPathName("en")}>
-          <Image
-            loading="lazy"
-            quality={1}
-            width={50}
-            className=" box-shadow"
-            height={50}
-            alt="flag"
-            src={`/images/en.svg`}
-          />
-        </Link>
-      </li>
-      <li>
-        <Link href={redirectedPathName("ar")}>
-          <Image
-            loading="lazy"
-            quality={1}
-            width={50}
-            height={50}
-            alt="flag"
-            className="shadow-xl "
-            src={`/images/ar.webp`}
-          />
-        </Link>
-      </li>
+    <div className="flex gap-3">
+      <Link href={redirectedPathName("en")}>
+        <Image
+          loading="lazy"
+          quality={1}
+          width={50}
+          className="w-14 h-9 object-cover"
+          height={50}
+          alt="flag"
+          src={`/images/en.svg`}
+        />
+      </Link>
+
+      <Link href={redirectedPathName("ar")}>
+        <Image
+          loading="lazy"
+          quality={40}
+          width={50}
+          height={50}
+          alt="flag"
+          className="w-14 h-9 object-cover"
+          src={`/images/ar.webp`}
+        />
+      </Link>
+
       <Link href={redirectedPathName("ru")}>
         <Image
           loading="lazy"
           quality={1}
           width={50}
           height={50}
-          className="shadow-xl"
+          className="w-14 h-9 object-cover"
           alt="flag"
           src={`/images/ru.svg`}
         />
       </Link>
-    </ul>
+    </div>
   );
 }
