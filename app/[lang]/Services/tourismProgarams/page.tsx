ada6f9b75c65f0b24c885a59deb235eaae85afcd
abdalraoufalsheikhnajeeb <abdalraoufalsheikhnajeeb@gmail.com>
2024-11-24 22:27:52 +0300

    d

--- app/[lang]/Services/tourismProgarams/page.tsx
+++ app/[lang]/Services/tourismProgarams/page.tsx
@@ -2,15 +2,23 @@ import { getDictionary } from "../../../../get-dictionary";
 import { Locale } from "../../../../i18n-config";
 import CountrySlider from "../../components/CountrySlider";
 import { tourismProgarams } from "../../data";
-export default async function page({
-  params: { lang },
+
+export default async function Page({
+  params,
 }: {
-  params: { lang: Locale };
+  params: Promise<{ lang: Locale }>;
 }) {
+  const { lang } = await params;
   const dic = await getDictionary(lang);
+
   return (
     <>
-      <CountrySlider dic={dic} title={dic.tourismProgarams} subtitle={dic.tourismProgaramsText} data={tourismProgarams} />
+      <CountrySlider
+        dic={dic}
+        title={dic.tourismProgarams}
+        subtitle={dic.tourismProgaramsText}
+        data={tourismProgarams}
+      />
     </>
   );
 }
